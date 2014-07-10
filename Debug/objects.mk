c6e08740ec6ba8d2b1a99073c6ef82d004af0842
Jonas Kunze <kunze.jonas@gmail.com>
2014-07-10 15:56:39 +0200

    cleanup

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_system -lboost_thread -lboost_program_options -lrt -lpthread -lboost_filesystem -ltcmalloc -lzmq
-
